5b4a57fc0a3f36a40542e73516743cbbe30344c6
woodwardtw <twwoodward@me.com>
2025-05-26 10:04:42 -0400

    attach gif to page for confirmation

--- js/gif-picker.js
+++ js/gif-picker.js
@@ -6,7 +6,6 @@ gifBtn.addEventListener('click', (event) => {
     // Your click handling code here
     event.preventDefault(); // Prevents form submission
     event.stopPropagation(); // Stops event bubbling
-    console.log('Element clicked');
     dialog.showModal();
 });
 
@@ -14,10 +13,18 @@ const confirm = document.querySelector('#confirm');
 confirm.addEventListener("click", () => {
 	event.preventDefault(); // Prevents form submission
 	event.stopPropagation(); // Stops event bubbling
+	const chosenGif = document.querySelector(".selected");
+	attachChosenGif(chosenGif);
 	dialog.close();
  });
 
-
+function attachChosenGif(chosenGif){
+	console.log(chosenGif)
+	const destination = document.querySelector("#field_2_14");
+	const img = document.createElement("img");
+	img.src = chosenGif.src;
+	destination.appendChild(img);		
+}
 
 fetch('../wp-json/wp/v2/media?mime_type=image/gif&per_page=100')
         .then(function (response) {
@@ -46,10 +53,9 @@ function makeGif(data){
 
 function imageClicker(){
 	const allImages = document.querySelectorAll(".gif");
-	const destination = document.querySelector("#input_1_10")
+	const destination = document.querySelector("#input_2_10"); //chat back to _1_10 for prod
 	allImages.forEach(function(gif){
 		gif.addEventListener('click', () => {		  
-		    console.log(gif.src)
 		    destination.value = gif.src;
 		    cleanSelected(allImages);
 		    gif.classList.toggle('selected');
