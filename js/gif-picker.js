3b357186bb120d64f20226e7bb98b0e80a44b1d4
woodwardtw <twwoodward@me.com>
2025-06-03 08:03:48 -0400

    better gif picker

--- js/gif-picker.js
+++ js/gif-picker.js
@@ -14,7 +14,7 @@ const confirm = document.querySelector('#confirm');
 confirm.addEventListener("click", () => {
 	event.preventDefault(); // Prevents form submission
 	event.stopPropagation(); // Stops event bubbling
-	const chosenGif = document.querySelector(".selected");
+	const chosenGif = document.querySelector(".selected img");
 	attachChosenGif(chosenGif);
 	dialog.close();
  });
@@ -37,6 +37,8 @@ function cleanOldGif(){
 
 }
 
+
+//gets all gifs from WP library
 fetch('../wp-json/wp/v2/media?mime_type=image/gif&per_page=100')
         .then(function (response) {
             return response.json();
@@ -50,34 +52,48 @@ fetch('../wp-json/wp/v2/media?mime_type=image/gif&per_page=100')
             console.log('error: ' + err);
         });
 
-
-function makeGif(data){
+//Makes the gifs for display
+function makeGif(data) {
 	const destination = document.querySelector("#gif-library");
-	data.forEach(function(gif){
-		if(gif.caption.rendered === ""){
+
+	data.forEach(function(gif) {
+		if (gif.caption.rendered === "") {
+			// Create wrapper div
+			const wrapper = document.createElement("div");
+			wrapper.classList.add("gif-frame");
+
+			// Create image
 			const img = document.createElement("img");
 			img.src = gif.guid.rendered;
-			img.classList.add('gif');
+			img.classList.add("gif");
 			img.dataset.id = gif.id;
-			destination.appendChild(img);	
-		}			
-	})
+
+			// Append image to wrapper
+			wrapper.appendChild(img);
+
+			// Append wrapper to destination
+			destination.appendChild(wrapper);
+		}
+	});
 }
+
 
 
 function imageClicker(){
 	const formId = document.querySelector(".gform_wrapper form").dataset.formid	
 	const urlField = `#input_${formId}_10`;
 	const imageIdField = `#input_${formId}_16`;
-	const allImages = document.querySelectorAll(".gif");	
+	const allImages = document.querySelectorAll(".gif-frame");	
 	const urlDestination = document.querySelector(urlField); //change between _2_10 & _1_10 for prod
 	const imageIdDestination = document.querySelector(imageIdField); //change 
-	allImages.forEach(function(gif){
-		gif.addEventListener('click', () => {		  
+	allImages.forEach(function(gifFrame){
+		gifFrame.addEventListener('click', () => {
+			gif = gifFrame.querySelector('.gif');		  
 		    urlDestination.value = gif.src;//value to url
+		   // console.log(gif.src)
 		    imageIdDestination.value = gif.dataset.id;//value to image ID
 		    cleanSelected(allImages);
-		    gif.classList.toggle('selected');
+		    gifFrame.classList.toggle('selected');
 		  });
 	})
 }
